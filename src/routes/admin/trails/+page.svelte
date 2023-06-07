23304e345ac376884ebb1bba3a9662bed47bdd12
devlulcas <lucasalvesrego.contato@gmail.com>
2023-06-07 02:59:12 -0300

    feat: move trail creation to its own rout

---
+++ src/routes/admin/trails/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { AdminSection } from '$lib/components/admin-section';
+	import { Button } from '$lib/components/button';
+	import { Input } from '$lib/components/input';
+</script>
+
+<AdminSection title="Gerenciar conteúdo">
+	<form class="lc-box flex flex-col gap-4" method="POST" action="/createTrail">
+		<h2 class="text-2xl font-bold">Criar trilha</h2>
+		<Input variant="opaque" id="title" label="Título" name="title" />
+		<Input variant="opaque" id="description" label="Descrição" name="description" />
+		<Input variant="opaque" id="image" label="Imagem" name="image" />
+		<Button type="submit">Criar</Button>
+	</form>
+</AdminSection>
