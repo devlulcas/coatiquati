712fa9542f705ed8ee7fc7dddc53585f187c7ce3
devlulcas <lucasalvesrego.contato@gmail.com>
2023-06-12 05:33:32 -0300

    feat: add className to section container

--- src/lib/components/admin-section/admin-section.svelte
+++ src/lib/components/admin-section/admin-section.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	export let title: string;
 	export let titleClassname = '';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
 </script>
 
-<section>
+<section class={className}>
 	<h2 class="text-2xl font-bold lc-box mt-6 mb-2 h-fit {titleClassname}">
 		{title}
 	</h2>
