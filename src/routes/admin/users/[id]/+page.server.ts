20292c72a2fd5ec0adff6182427568fcca721d0f
devlulcas <lucasalvesrego.contato@gmail.com>
2023-06-14 08:18:41 -0300

    refactor: remove prisma deps

--- src/routes/admin/users/[id]/+page.server.ts
+++ src/routes/admin/users/[id]/+page.server.ts
@@ -1,9 +1,9 @@
-import { PrismaUserRepository } from '$src/modules/user/repositories/postgres-user.repository';
+import { redirectToSignIn } from '$lib/utils/redirect-url';
+import { Roles, userRolesHasRole } from '$src/modules/user/constants/user-roles';
+import { PostgresUserRepository } from '$src/modules/user/repositories/postgres-user.repository';
 import { GetUserProfile } from '$src/modules/user/use-cases/get-user-profile';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { Roles, userRolesHasRole } from '$src/modules/user/constants/user-roles';
-import { redirectToSignIn } from '$lib/utils/redirect-url';
 
 export const load: PageServerLoad = async ({ locals, params, url }) => {
 	const session = await locals.auth.validateUser();
@@ -12,7 +12,7 @@ export const load: PageServerLoad = async ({ locals, params, url }) => {
 		throw redirectToSignIn(url.pathname);
 	}
 
-	const getUserProfile = new GetUserProfile(new PrismaUserRepository());
+	const getUserProfile = new GetUserProfile(new PostgresUserRepository());
 
 	const currentUser = await getUserProfile.execute(session.user.id);
 
