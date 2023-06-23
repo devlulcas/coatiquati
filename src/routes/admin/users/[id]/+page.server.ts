2b514cb6bad7f9b268e4c5f04485c0b7572c8b66
devlulcas <lucasalvesrego.contato@gmail.com>
2023-06-22 22:21:43 -0300

    feat: initial ban ui on user page

--- src/routes/admin/users/[id]/+page.server.ts
+++ src/routes/admin/users/[id]/+page.server.ts
@@ -1,9 +1,11 @@
 import { redirectToSignIn } from '$lib/utils/redirect-url';
+import { PostgresBanRegistryRepository } from '$src/modules/ban/repositories/postgres-ban-registry.repository';
+import { BanUser } from '$src/modules/ban/use-cases/ban-user';
 import { Roles, userRolesHasRole } from '$src/modules/user/constants/user-roles';
 import { PostgresUserRepository } from '$src/modules/user/repositories/postgres-user.repository';
 import { GetUserProfile } from '$src/modules/user/use-cases/get-user-profile';
 import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals, params, url }) => {
 	const session = await locals.auth.validateUser();
@@ -31,3 +33,59 @@ export const load: PageServerLoad = async ({ locals, params, url }) => {
 		currentUserIsAdmin: userRolesHasRole(Roles.ADMIN, currentUser.data.roles)
 	};
 };
+
+export const actions = {
+	ban: async ({ params, locals, url, request }) => {
+		const session = await locals.auth.validateUser();
+
+		if (!session.user) {
+			throw redirectToSignIn(url.pathname);
+		}
+
+		const getUserProfile = new GetUserProfile(new PostgresUserRepository());
+
+		const currentUser = await getUserProfile.execute(session.user.id);
+
+		if (currentUser.error) {
+			throw redirectToSignIn(url.pathname);
+		}
+
+		const user = await getUserProfile.execute(params.id);
+
+		if (user.error) {
+			throw error(404, 'User not found');
+		}
+
+		if (!userRolesHasRole(Roles.ADMIN, currentUser.data.roles)) {
+			throw error(403, 'Forbidden');
+		}
+
+		if (userRolesHasRole(Roles.ADMIN, user.data.roles)) {
+			throw error(403, 'Forbidden');
+		}
+
+		if (user.data.active === false) {
+			throw error(403, 'Forbidden');
+		}
+
+		const form = await request.formData();
+
+		const banUser = new BanUser(new PostgresUserRepository(), new PostgresBanRegistryRepository());
+
+		const banResult = await banUser.execute({
+			userId: user.data.id,
+			adminId: currentUser.data.id,
+			reason: form.get('reason')?.toString() || 'Não informado'
+		});
+
+		console.log(banResult);
+
+		if (banResult.error) {
+			throw error(403, banResult.error.message);
+		}
+
+		return {
+			data: banResult.data
+		};
+	}
+} satisfies Actions;
--- src/routes/admin/users/[id]/+page.svelte
+++ src/routes/admin/users/[id]/+page.svelte
@@ -2,9 +2,12 @@
 	import { AdminSection } from '$lib/components/admin-section';
 	import { Badge } from '$lib/components/badge';
 	import { Button } from '$lib/components/button';
-	import type { PageServerData } from './$types';
+	import type { PageServerData, ActionData } from './$types';
+	import { enhance } from '$app/forms';
+	import { InputContainer } from '$lib/components/input-container/';
 
 	export let data: PageServerData;
+	export let form: ActionData;
 
 	const intl = new Intl.ListFormat('pt-BR', { style: 'long', type: 'conjunction' });
 </script>
@@ -40,7 +43,7 @@
 		</p>
 	</main>
 
-	<form method="POST" class="mt-4 flex gap-2 w-full">
+	<form method="POST" use:enhance class="mt-4 flex gap-2 w-full">
 		{#if data.user.roles.includes('admin')}
 			<Badge variant="warning">
 				Atenção: este usuário é um administrador e tem acesso a todas as funcionalidades do sistema.
@@ -67,4 +70,42 @@
 			</Badge>
 		{/if}
 	</form>
+
+	<form action="?/ban" method="POST" use:enhance class="lc-box mt-4 flex flex-col gap-2 w-full">
+		<InputContainer variant="opaque" let:inputClassName label="Motivo do banimento" id="reason">
+			<input class={inputClassName} type="text" name="reason" id="reason" />
+		</InputContainer>
+
+		<Badge variant="error">
+			Banir usuário ({data.user.banVotes} / 2)
+			<Button
+				class="min-w-fit ml-auto hover:bg-white hover:text-red-500"
+				type="submit"
+				variant="ghost"
+			>
+				{data.user.banVotes === 0 ? 'Iniciar banimento' : 'Confirmar banimento'}
+			</Button>
+		</Badge>
+	</form>
+
+	<div class="flex flex-col gap-2 mt-2">
+		{#if form?.data}
+			<Badge variant="success">
+				{form.data.first.username} iniciou um processo de banimento contra {form.data.target
+					.username}.
+			</Badge>
+
+			{#if form.data.second}
+				<Badge variant="success">
+					{form.data.second.username} confirmou o banimento.
+				</Badge>
+			{/if}
+
+			{#if form.data.reason}
+				<Badge variant="info">
+					Motivo: {form.data.reason}
+				</Badge>
+			{/if}
+		{/if}
+	</div>
 </AdminSection>
