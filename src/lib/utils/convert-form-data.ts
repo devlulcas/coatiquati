318041ad4cd34d85fe68f6b017f99a90211be334
devlulcas <lucasalvesrego.contato@gmail.com>
2023-06-27 09:01:50 -0300

    feat: formData to object converter

--- src/lib/utils/convert-form-data.ts
+++ src/lib/utils/convert-form-data.ts
@@ -1,7 +1,7 @@
 export function formDataToObject(formData: FormData) {
 	const entries = formData.entries();
 
-	const data: Record<string, unknown> = {};
+	const data: Record<string, any> = {};
 
 	for (const [key, value] of entries) {
 		data[key] = value;
