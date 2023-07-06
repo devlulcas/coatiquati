5a6d3032c139a7a605b4ce02c329f6cf947dfe59
devlulcas <lucasalvesrego.contato@gmail.com>
2023-07-06 01:41:29 -0300

    fix: limit badge height

--- src/lib/components/badge/badge.variants.ts
+++ src/lib/components/badge/badge.variants.ts
@@ -1,25 +1,22 @@
 import { cva } from 'class-variance-authority';
 
-export const badgeVariants = cva(
-	'text-white w-full p-2 rounded-md flex items-center gap-2 font-semibold',
-	{
-		variants: {
-			variant: {
-				default: 'bg-gray-500/75',
-				error: 'bg-red-500/75',
-				warning: 'bg-yellow-500/75',
-				info: 'bg-blue-500/75',
-				success: 'bg-green-500/75'
-			},
-			size: {
-				default: 'text-sm',
-				sm: 'text-xs',
-				lg: 'text-lg'
-			}
+export const badgeVariants = cva('text-white h-fit w-full p-2 rounded-md flex items-center gap-2 font-semibold', {
+	variants: {
+		variant: {
+			default: 'bg-gray-500/75',
+			error: 'bg-red-500/75',
+			warning: 'bg-yellow-500/75',
+			info: 'bg-blue-500/75',
+			success: 'bg-green-500/75'
 		},
-		defaultVariants: {
-			size: 'default',
-			variant: 'default'
+		size: {
+			default: 'text-sm',
+			sm: 'text-xs',
+			lg: 'text-lg'
 		}
+	},
+	defaultVariants: {
+		size: 'default',
+		variant: 'default'
 	}
-);
+});
