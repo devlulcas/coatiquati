94d60de6933a6a8f65b292a154a7bbca30d198ca
devlulcas <lucasalvesrego.contato@gmail.com>
2023-07-13 23:37:08 -0300

    deps: remove carousel

--- src/lib/components/carousel/index.ts
+++
@@ -1,8 +0,0 @@
-import { default as CarouselContainer } from './carousel-container.svelte';
-import { default as CarouselItem } from './carousel-item.svelte';
-
-// Grouped carousel components
-export const Carousel = {
-  Container: CarouselContainer,
-  Item: CarouselItem,
-}
