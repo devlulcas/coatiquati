2367bdb19ec506d7969706155e945ea8318bfa0c
devlulcas <lucasalvesrego.contato@gmail.com>
2023-07-16 23:28:05 -0300

    chore: migrate cjs files to esm

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,16 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			entries: ['*']
+		},
 		alias: {
-			$lib: './src/lib',
-			$modules: './src/modules',
-			$src: './src'
+			'$modules/*': './src/modules',
+			'$lib/*': './src/lib',
+			'$/*': './src/*'
 		}
 	}
 };
