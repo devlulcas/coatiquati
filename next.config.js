b7052a11c59c96158d3fcb9d4ebfa1bbc23e00c5
devlulcas <lucasalvesrego.contato@gmail.com>
2023-08-18 06:04:21 -0300

    feat: opt-in into server actions

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'res.cloudinary.com', 'placekitten.com'],
   },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
