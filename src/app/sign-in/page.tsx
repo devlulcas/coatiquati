b9b60586e0659861818c100fa729f288dd1dc521
devlulcas <lucasalvesrego.contato@gmail.com>
2023-08-18 06:25:25 -0300

    refactor: renamed helpers folder to utils

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import { SignInForm } from '@/modules/auth/components/sign-in-form';
-import { getPageSession } from '@/modules/auth/helpers/get-page-session';
+import { getPageSession } from '@/modules/auth/utils/get-page-session';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,5 @@
 import { SignUpForm } from '@/modules/auth/components/sign-up-form';
-import { getPageSession } from '@/modules/auth/helpers/get-page-session';
+import { getPageSession } from '@/modules/auth/utils/get-page-session';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
