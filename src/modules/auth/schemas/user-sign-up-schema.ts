fe60f8a98df50b1f397da6df02a502e5fb0ae68f
devlulcas <lucasalvesrego.contato@gmail.com>
2023-08-18 06:26:59 -0300

    feat: better signUp error messages

--- src/modules/auth/schemas/user-sign-up-schema.ts
+++ src/modules/auth/schemas/user-sign-up-schema.ts
@@ -1,7 +1,15 @@
 import { z } from 'zod';
 
 export const userSignUpSchema = z.object({
-  username: z.string().min(4).max(31),
-  password: z.string().min(6).max(255),
-  email: z.string().min(6).max(255),
+  username: z
+    .string({ required_error: 'O nome de usuário é obrigatório' })
+    .min(4, { message: 'O nome de usuário deve ter pelo menos 4 caracteres' })
+    .max(31, { message: 'O nome de usuário deve ter no máximo 31 caracteres' }),
+  password: z
+    .string({ required_error: 'A senha é obrigatória' })
+    .min(6, { message: 'A senha deve ter pelo menos 6 caracteres' })
+    .max(255, { message: 'A senha deve ter no máximo 255 caracteres' }),
+  email: z.string({ required_error: 'O email é obrigatório' }).email({
+    message: 'O email deve ser válido',
+  }),
 });
