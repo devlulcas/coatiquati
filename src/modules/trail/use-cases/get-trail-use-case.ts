3d52b40b64273dcc7d3931fd05a3833df6439086
devlulcas <lucasalvesrego.contato@gmail.com>
2023-08-19 00:08:27 -0300

    feat: bring trail author with the rest of the data

--- src/modules/trail/use-cases/get-trail-use-case.ts
+++ src/modules/trail/use-cases/get-trail-use-case.ts
@@ -1,7 +1,9 @@
 import { db } from '@/modules/database/db';
 import { topicTable } from '@/modules/database/schema/topic';
 import { trailTable } from '@/modules/database/schema/trail';
+import { userTable } from '@/modules/database/schema/user';
 import type { Topic } from '@/modules/topic/types/topic';
+import type { User } from '@/modules/user/types/user';
 import { eq } from 'drizzle-orm';
 import { z } from 'zod';
 import { type Trail } from '../types/trail';
@@ -14,6 +16,7 @@ type GetTrailUseCaseSchema = z.infer<typeof getTrailUseCaseSchema>;
 
 type Result = {
   trail: Trail;
+  author: User;
   topics: Topic[];
 } | null;
 
@@ -31,26 +34,26 @@ export async function getTrailUseCase(
       .select()
       .from(trailTable)
       .where(eq(trailTable.id, params.id))
+      .innerJoin(userTable, eq(trailTable.authorId, userTable.id))
       .leftJoin(topicTable, eq(trailTable.id, topicTable.trailId))
       .all();
 
     const trail = data.at(0)?.trail;
 
-    if (!trail) return null;
+    const author = data.at(0)?.user;
+
+    if (!trail || !author) return null;
 
     const topics: Topic[] = [];
 
     for (const item of data) {
       if (!item.topic) continue;
-
-      if (topics.find((topic) => topic.id === item.topic?.id)) {
-        continue;
-      }
+      if (topics.find((topic) => topic.id === item.topic?.id)) continue;
 
       topics.push(item.topic);
     }
 
-    return { trail, topics };
+    return { trail, topics, author };
   } catch (error) {
     console.error(error);
     throw new Error('Erro ao buscar trilha');
