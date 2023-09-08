5c46d49bf97c7b692dd00e9d9063f782f2843173
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-08 01:27:50 -0300

    feat: debounce hooks

---
+++ src/shared/hooks/use-debounced-state.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export function useDebouncedState<T>(
+  value: T,
+  delay: number
+): [T, (value: T) => void] {
+  const [state, setState] = useState(value);
+  const debouncedState = useDebounce(state, delay);
+  return [debouncedState, setState];
+}
