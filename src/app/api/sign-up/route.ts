a5a340211339c09283d7ba0989d9c1df75b1efb2
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-07 23:28:33 -0300

    fix: change email_verified to camel case

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -29,7 +29,7 @@ export const POST = async (request: NextRequest) => {
         username,
         email,
         role: roles.USER,
-        email_verified: false,
+        emailVerified: null,
       },
     });
 
--- src/modules/auth/services/lucia.ts
+++ src/modules/auth/services/lucia.ts
@@ -21,7 +21,7 @@ export const auth = lucia({
       username: data.username,
       role: data.role,
       email: data.email,
-      emailVerified: data.email_verified,
+      emailVerified: data.emailVerified,
     };
   },
   getSessionAttributes: (data) => {
