8764ee92b9b1a6b4ea220c6aa92e3976d1d1a2bb
lucas rego <lucasalvesrego.contato@gmail.com>
2023-09-14 15:12:59 -0300

    feat: video uploader

---
+++ src/modules/content/components/new-video-content-dialog-trigger/index.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Button } from '@/shared/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from '@/shared/components/ui/dialog';
+import { VideoIcon } from 'lucide-react';
+import { VideoContentForm } from './video-content-form';
+
+export function NewVideoContentDialogTrigger() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="flex gap-2 items-center justify-center">
+          <VideoIcon /> Contribuir com um vídeo
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="min-w-fit">
+        <DialogHeader>
+          <VideoContentForm onSubmit={(data) => console.log(data)} />
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ src/modules/content/components/new-video-content-dialog-trigger/video-content-form.tsx
@@ -0,0 +1,148 @@
+'use client';
+
+import { Button } from '@/shared/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/shared/components/ui/form';
+import { Input } from '@/shared/components/ui/input';
+import { cn } from '@/shared/utils/cn';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
+import { z } from 'zod';
+
+const videoContentSchema = z.object({
+  url: z
+    .string()
+    .url()
+    .refine((url) => {
+      try {
+        const urlObject = new URL(url);
+
+        const validHostnames = ['www.youtube.com', 'youtube.com', 'youtu.be'];
+
+        return validHostnames.includes(urlObject.hostname);
+      } catch (error) {
+        return false;
+      }
+    }),
+  title: z.string(),
+  alt: z.string(),
+});
+
+function extractYoutubeVideoId(url: string) {
+  try {
+    const urlObject = new URL(url);
+
+    if (urlObject.hostname === 'youtu.be') {
+      return urlObject.pathname.replace('/', '');
+    }
+
+    return urlObject.searchParams.get('v');
+  } catch (error) {
+    return null;
+  }
+}
+
+type VideoContentSchema = z.infer<typeof videoContentSchema>;
+
+type VideoContentFormProps = {
+  onSubmit: (data: VideoContentSchema) => void | Promise<void>;
+  defaultValues?: Partial<VideoContentSchema>;
+  className?: string;
+};
+
+export function VideoContentForm({
+  onSubmit,
+  className,
+  defaultValues,
+}: VideoContentFormProps) {
+  const form = useForm<VideoContentSchema>({
+    resolver: zodResolver(videoContentSchema),
+    defaultValues: defaultValues,
+  });
+
+  const url = form.watch('url');
+
+  const youtubeId = extractYoutubeVideoId(url);
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn(
+          'flex h-full flex-col gap-4 p-4 border rounded-lg',
+          className
+        )}
+        action="/api/sign-in"
+      >
+        <div
+          className={cn(
+            'aspect-w-16 aspect-h-9 rounded-lg overflow-hidden min-w-0',
+            youtubeId ? 'bg-black' : 'bg-gray-200'
+          )}
+        >
+          {youtubeId && (
+            <LiteYouTubeEmbed id={youtubeId} title={form.watch('title')} />
+          )}
+        </div>
+
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>URL do vídeo no Youtube</FormLabel>
+              <FormControl>
+                <Input
+                  type="url"
+                  placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="alt"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Texto alternativo</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Rick roll" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Título</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Gato" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="w-full mt-auto" type="submit">
+          Salvar vídeo
+        </Button>
+      </form>
+    </Form>
+  );
+}
