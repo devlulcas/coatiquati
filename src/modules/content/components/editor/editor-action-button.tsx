3517f61197e728016bd69b130b7cce9b5a767a09
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-14 08:06:14 -0300

    feat: accept external classNames  for  EditorActionButton

--- src/modules/content/components/editor/editor-action-button.tsx
+++ src/modules/content/components/editor/editor-action-button.tsx
@@ -2,12 +2,14 @@
 
 import { Button } from '@/shared/components/ui/button';
 import { cn } from '@/shared/utils/cn';
+import type { ClassValue } from 'clsx';
 
 type EditorActionButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   icon: React.ReactNode;
   label: string;
   disabled?: boolean;
   active?: boolean;
+  className?: ClassValue;
 };
 
 export function EditorActionButton({
@@ -16,12 +18,17 @@ export function EditorActionButton({
   onClick,
   disabled,
   active,
+  className,
 }: EditorActionButtonProps) {
   return (
     <Button
       onClick={onClick}
       disabled={disabled}
-      className={cn('gap-2', { 'bg-brand-500 text-brand-50': active })}
+      className={cn(
+        'gap-2 flex-1 whitespace-nowrap',
+        { 'bg-brand-500 text-brand-50': active },
+        className
+      )}
     >
       {icon}
       {label}
