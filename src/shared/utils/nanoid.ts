0f9ade5eef803bba8c7a3d0a1958bba76dd3ca06
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-14 08:03:43 -0300

    feat: nano id utility

---
+++ src/shared/utils/nanoid.ts
@@ -0,0 +1,12 @@
+export function nanoid(length = 21) {
+  const alphabet =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  let id = '';
+
+  for (let i = 0; i < length; i++) {
+    id += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+  }
+
+  return id;
+}
