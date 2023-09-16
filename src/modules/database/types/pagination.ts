a588c183f0621ccc9f9c04eeceb8b5dfdefdb059
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-16 08:12:45 -0300

    feat: createPaginationSchemaWithSearch

--- src/modules/database/types/pagination.ts
+++ src/modules/database/types/pagination.ts
@@ -1,13 +1,24 @@
 import { z } from 'zod';
 
-export const createPaginationSchema = (
-  take: number,
-  skip: number,
-) => {
+export const createPaginationSchema = (take = 10, skip = 0) => {
   return z.object({
     take: z.number().optional().default(take),
     skip: z.number().optional().default(skip),
   });
 };
 
-export type PaginationSchema = z.infer<ReturnType<typeof createPaginationSchema>>;
+export const createPaginationSchemaWithSearch = (take = 10, skip = 0) => {
+  return createPaginationSchema(take, skip).merge(
+    z.object({
+      search: z.string().optional().default(''),
+    })
+  );
+};
+
+export type PaginationSchema = z.infer<
+  ReturnType<typeof createPaginationSchema>
+>;
+
+export type PaginationSchemaWithSearch = z.infer<
+  ReturnType<typeof createPaginationSchemaWithSearch>
+>;
