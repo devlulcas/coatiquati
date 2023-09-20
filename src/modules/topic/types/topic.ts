42b9076ed110979da3757dac0d14b7dd5a726eb7
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-20 07:26:06 -0300

    fix: topic contains the trail id

--- src/modules/topic/types/topic.ts
+++ src/modules/topic/types/topic.ts
@@ -6,7 +6,7 @@ import type {
 import type { Creatable, Updatable } from '@/modules/database/types/utils';
 import type { Contributor } from '@/modules/user/types/user';
 
-export type Topic = Omit<TopicTable, 'authorId' | 'trailId'> & {
+export type Topic = Omit<TopicTable, 'authorId'> & {
   contributors: { user: Contributor }[];
   author: Contributor;
 };
