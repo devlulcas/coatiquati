6676546cae43f0dc3820ad5be3bdbbdee47c3133
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-30 21:21:51 -0300

    feat: add process.env.NODE_ENV

--- src/env.ts
+++ src/env.ts
@@ -4,11 +4,13 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string(),
+    NODE_ENV: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_WEBSITE: z.string().min(1),
   },
   runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_WEBSITE: process.env.NEXT_PUBLIC_WEBSITE,
   },
