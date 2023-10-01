fbb320af8addb7b0b0ecfab73527839e312e627f
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-30 21:58:50 -0300

    revert: edge runtime is bad for my mental health (https://github.com/lucia-auth/lucia/issues/1063#issuecomment-1706370724)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,14 @@
+import { protectWithRedirect } from '@/modules/auth/utils/protect-with-redirect';
+
 type DashboardLayoutProps = {
   children: React.ReactNode;
 };
 
 export default async function DashboardLayout({ children }: DashboardLayoutProps) {
+  await protectWithRedirect({
+    redirectTo: '/sign-in',
+    acceptRoles: ['ADMIN', 'HIGH_PRIVILEGE_ADMIN'],
+  });
+
   return <div className="py-8 container">{children}</div>;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,3 @@
-import { NextResponse, type NextRequest } from 'next/server';
-import { roles } from './modules/auth/constants/roles';
-import { auth } from './modules/auth/services/lucia';
+import { type NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    const authRequest = auth.handleRequest(request);
-
-    const session = await authRequest.validate();
-
-    if (session === null) {
-      return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
-
-    const acceptRoles: string[] = [roles.ADMIN, roles.USER];
-
-    if (!acceptRoles.includes(session.user.role)) {
-      return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
-
-    return NextResponse.next();
-  }
-
-  return NextResponse.next();
-}
+export async function middleware(request: NextRequest) {}
