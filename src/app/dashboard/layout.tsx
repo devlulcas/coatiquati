643f2208764e2b21f305b0b425ee68398b4c1561
devlulcas <lucasalvesrego.contato@gmail.com>
2023-09-30 21:18:04 -0300

    feat: move dashboard auth to middleware to avoid caching problems

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,7 @@
-import { protectWithRedirect } from '@/modules/auth/utils/protect-with-redirect';
-
 type DashboardLayoutProps = {
   children: React.ReactNode;
 };
 
 export default async function DashboardLayout({ children }: DashboardLayoutProps) {
-  await protectWithRedirect({
-    redirectTo: '/sign-in',
-    acceptRoles: ['ADMIN'],
-  });
-
   return <div className="py-8 container">{children}</div>;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,25 @@
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
+import { roles } from './modules/auth/constants/roles';
+import { auth } from './modules/auth/services/lucia';
 
-export async function middleware(request: NextRequest) {}
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    const authRequest = auth.handleRequest(request);
+
+    const session = await authRequest.validate();
+
+    if (session === null) {
+      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+
+    const acceptRoles: string[] = [roles.ADMIN, roles.USER];
+
+    if (!acceptRoles.includes(session.user.role)) {
+      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
