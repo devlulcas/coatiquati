45f7fb3ad322d6b0d793b24428167b6223a2e7ef
lucas rego <lucasalvesrego.contato@gmail.com>
2023-10-03 17:48:20 -0300

    feat: isolate the renderer of the rte

---
+++ src/modules/content/components/editor/editor-content.tsx
@@ -0,0 +1,10 @@
+import { Editor } from '@tiptap/react';
+import { EditorContent as TipTapEditorContent } from '@tiptap/react';
+
+type EditorContentProps = {
+  editor: Editor | null;
+};
+
+export function EditorContent({ editor }: EditorContentProps) {
+  return <TipTapEditorContent editor={editor} className="prose dark:prose-invert" />;
+}
