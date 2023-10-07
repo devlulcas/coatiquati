88852c2fd6097180626bb5cec634780623fc7283
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-07 17:12:17 -0300

    feat: add more data to a rte content

--- src/app/contents/post/[postId]/page.tsx
+++ src/app/contents/post/[postId]/page.tsx
@@ -1,5 +1,6 @@
 import { PreviewRichTextContent } from '@/modules/content/components/preview-rich-text-content';
 import { getRichTextContentUseCase } from '@/modules/content/use-cases/get-rich-text-content-use-case';
+import { TrailHeading } from '@/modules/trail/components/trail-heading';
 
 type PageProps = {
   params: {
@@ -14,7 +15,9 @@ export default async function Page({ params }: PageProps) {
 
   return (
     <div className="py-8 container">
-      <PreviewRichTextContent richTextContent={postData.asJson} />
+      <TrailHeading trail={postData.trail} />
+      <h1 className="text-4xl font-bold">{postData.topic.title}</h1>
+      <PreviewRichTextContent richTextContent={postData.content.asJson} />
     </div>
   );
 }
--- src/modules/content/use-cases/get-rich-text-content-use-case.ts
+++ src/modules/content/use-cases/get-rich-text-content-use-case.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod';
 
+import { DrizzleTopicRepository } from '@/modules/topic/repositories/topic-repository';
+import type { Topic } from '@/modules/topic/types/topic';
+import { DrizzleTrailRepository } from '@/modules/trail/repositories/trail-repository';
+import type { Trail } from '@/modules/trail/types/trail';
+import { DrizzleContentRepository } from '../repositories/content-repository';
 import { DrizzleRichTextContentRepository } from '../repositories/rich-text-content-repository';
 import type { ContentRichText } from '../types/content';
 
@@ -9,17 +14,30 @@ const getRichTextContentUseCaseSchema = z.object({
 
 type GetRichTextContentUseCaseSchema = z.infer<typeof getRichTextContentUseCaseSchema>;
 
-export async function getRichTextContentUseCase(params: GetRichTextContentUseCaseSchema): Promise<ContentRichText> {
+export async function getRichTextContentUseCase(params: GetRichTextContentUseCaseSchema): Promise<{
+  content: ContentRichText;
+  trail: Trail;
+  topic: Topic;
+}> {
   const validatedParams = getRichTextContentUseCaseSchema.safeParse(params);
 
   if (!validatedParams.success) {
     throw new Error('Parâmetros inválidos');
   }
 
-  const repository = new DrizzleRichTextContentRepository();
+  const textContentRepository = new DrizzleRichTextContentRepository();
+  const topicRepository = new DrizzleTopicRepository(new DrizzleContentRepository());
+  const trailRepository = new DrizzleTrailRepository();
 
   try {
-    return repository.getContent(validatedParams.data.id);
+    const [content, topic] = await Promise.all([
+      textContentRepository.getContent(validatedParams.data.id),
+      topicRepository.getTopicById(validatedParams.data.id),
+    ]);
+
+    const trail = await trailRepository.getTrailById(topic.trailId);
+
+    return { content, trail, topic };
   } catch (error) {
     console.error(error);
     throw new Error('Erro ao buscar conteúdo');
