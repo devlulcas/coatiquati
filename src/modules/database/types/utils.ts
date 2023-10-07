ea065eea14261411fbdd88a986b5e7a6b0450068
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-07 16:59:35 -0300

    remove: updated at in the updatable data type

--- src/modules/database/types/utils.ts
+++ src/modules/database/types/utils.ts
@@ -1,6 +1,5 @@
 export type Creatable<T> = Omit<T, 'id' | 'createdAt' | 'updatedAt'>;
 
-export type Updatable<T> = Partial<Omit<T, 'createdAt'>> & {
+export type Updatable<T> = Partial<Creatable<T>> & {
   id: T extends { id: infer U } ? U : never;
-  updatedAt: string;
 };
