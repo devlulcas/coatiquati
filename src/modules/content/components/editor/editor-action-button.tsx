981a108b33fe4c510783fd49468c490917b29a20
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-09 02:29:11 -0300

    feat: show helper text only in the bigger screen

--- src/modules/content/components/editor/editor-action-button.tsx
+++ src/modules/content/components/editor/editor-action-button.tsx
@@ -12,26 +12,15 @@ type EditorActionButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   className?: ClassValue;
 };
 
-export function EditorActionButton({
-  icon,
-  label,
-  onClick,
-  disabled,
-  active,
-  className,
-}: EditorActionButtonProps) {
+export function EditorActionButton({ icon, label, onClick, disabled, active, className }: EditorActionButtonProps) {
   return (
     <Button
       onClick={onClick}
       disabled={disabled}
-      className={cn(
-        'gap-2 flex-1 whitespace-nowrap',
-        { 'bg-brand-500 text-brand-50': active },
-        className,
-      )}
+      className={cn('gap-2 flex-1 min-w-fit', { 'bg-brand-500 text-brand-50': active }, className)}
     >
       {icon}
-      {label}
+      <span className="sr-only whitespace-nowrap min-w-fit lg:not-sr-only">{label}</span>
     </Button>
   );
 }
