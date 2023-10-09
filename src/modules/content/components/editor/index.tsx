395ff219ff6b463146ed483c141a9edd72b0c3e0
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-09 02:28:45 -0300

    remove: take out the possibility to contribute with images inside the text editor

--- src/modules/content/components/editor/index.tsx
+++ src/modules/content/components/editor/index.tsx
@@ -3,10 +3,7 @@
 import { Button } from '@/shared/components/ui/button';
 import { useMutation } from '@tanstack/react-query';
 import { useRichTextEditor } from '../../hooks/use-rich-text-editor';
-import { NewImageContentDialogTrigger } from '../new-image-content-dialog-trigger';
-import { NewVideoContentDialogTrigger } from '../new-video-content-dialog-trigger';
 import { EditorContent } from './editor-content';
-import css from './editor.module.css';
 import { MenuBar } from './menu-bar';
 
 type TextEditorProps = {
@@ -34,25 +31,18 @@ export function TextEditor({ initialContent }: TextEditorProps) {
   };
 
   return (
-    <div className={css.editor}>
+    <div className="w-full flex flex-col gap-2">
       <MenuBar editor={editor} />
+
       <EditorContent editor={editor} />
 
-      <div className="mt-4 flex gap-2">
-        <Button
-          onClick={saveManually}
-          isLoading={saveMutation.isLoading}
-          variant={saveMutation.isError ? 'destructive' : 'default'}
-        >
-          {saveMutation.isError ? 'Erro ao salvar' : 'Salvar manualmente'}
-        </Button>
-
-        <NewImageContentDialogTrigger />
-
-        <NewVideoContentDialogTrigger />
-      </div>
-
-      <pre>{JSON.stringify(editor?.getJSON(), null, 2)}</pre>
+      <Button
+        onClick={saveManually}
+        isLoading={saveMutation.isLoading}
+        variant={saveMutation.isError ? 'destructive' : 'default'}
+      >
+        {saveMutation.isError ? 'Erro ao salvar' : 'Salvar manualmente'}
+      </Button>
     </div>
   );
 }
