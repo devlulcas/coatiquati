0d0a1680b087e09659b399f646491e6a90f5cb2e
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-09 02:26:29 -0300

    feat: improved the css for the editor

--- src/modules/content/components/editor/editor-content.tsx
+++ src/modules/content/components/editor/editor-content.tsx
@@ -1,9 +1,11 @@
+import { cn } from '@/shared/utils/cn';
 import { Editor, EditorContent as TipTapEditorContent } from '@tiptap/react';
+import css from './editor-content.module.css';
 
 type EditorContentProps = {
   editor: Editor | null;
 };
 
 export function EditorContent({ editor }: EditorContentProps) {
-  return <TipTapEditorContent editor={editor} className="prose dark:prose-invert" />;
+  return <TipTapEditorContent editor={editor} className={cn('prose dark:prose-invert', css.editor)} />;
 }
