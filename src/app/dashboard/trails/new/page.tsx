4cc1626bdab80b42636996e2ee1855b243071093
lucas rego <lucasalvesrego.contato@gmail.com>
2023-10-13 18:35:54 -0300

    feat: adds bg to new trail page

--- src/app/dashboard/trails/new/page.tsx
+++ src/app/dashboard/trails/new/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default async function Page() {
   return (
-    <div>
+    <div className='bg-background/75 border p-4 rounded backdrop-blur-md'>
       <NewTrailForm />
 
       <Button asChild className="mt-2" variant="secondary">
