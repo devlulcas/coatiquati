8454c3455d1cd72de128caeb0e975e977ff51eff
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-12 22:52:29 -0300

    remove: perf mode is kinda useless

--- src/modules/layout/components/main-layout-wrapper/index.tsx
+++ src/modules/layout/components/main-layout-wrapper/index.tsx
@@ -1,18 +1,7 @@
-'use client';
-
-import { cn } from '@/shared/utils/cn';
-import { usePerfStore } from '../../stores/perf-mode-store';
-
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
 export function MainLayoutWrapper({ children }: MainLayoutProps) {
-  const perfMode = usePerfStore(state => state.perfMode);
-
-  return (
-    <div className={cn('min-h-[100dvh]', perfMode ? 'bg-background/75' : 'backdrop-blur-xl')}>
-      {children}
-    </div>
-  );
+  return <div className="min-h-[100dvh] backdrop-blur-xl">{children}</div>;
 }
