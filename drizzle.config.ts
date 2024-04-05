ad21cf010d81b4240d6130a62d0eb39fec5a7b1a
devlulcas <lucasalvesrego.contato@gmail.com>
2024-04-05 08:39:51 -0300

    feat: finished libsql migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,16 +6,20 @@ config({
 });
 
 const DATABASE_URL = process.env.DATABASE_URL;
-
+const DATABASE_AUTH_TOKEN = process.env.DATABASE_AUTH_TOKEN;
 if (!DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined');
+}
+if (!DATABASE_AUTH_TOKEN) {
+  throw new Error('DATABASE_AUTH_TOKEN is not defined');
 }
 
 export default {
   schema: './src/modules/database/schema/*',
   out: './drizzle',
-  driver: 'better-sqlite',
+  driver: 'turso',
   dbCredentials: {
     url: DATABASE_URL,
+    authToken: DATABASE_AUTH_TOKEN,
   },
 } satisfies Config;
