49b089cf1df210ceae396abcb5763e2e2efc8ca9
devlulcas <lucasalvesrego.contato@gmail.com>
2024-04-07 06:03:06 -0300

    feat: show followers in profile

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -3,6 +3,7 @@ import { getMostSubscribedCategory } from '@/modules/trail-subscriptions/lib/get
 import { TrailCard } from '@/modules/trail/components/trail-card';
 import { getUserProfileQuery } from '@/modules/user/actions/get-user-profile-query';
 import { ProfileHeading } from '@/modules/user/components/profile-heading';
+import { UserAvatar } from '@/shared/components/common/user-avatar';
 import { StarsIcon } from 'lucide-react';
 
 type PageProps = {
@@ -38,6 +39,34 @@ export default async function Page({ params }: PageProps) {
       )}
 
       <section className="mt-8">
+        <h2 className="text-xl font-bold">Seguidores</h2>
+
+        {profile.followers.length === 0 && <p>Parece que {profile.username} ainda não tem seguidores.</p>}
+
+        <ul className="mt-4 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
+          {profile.followers.map(follower => (
+            <li key={follower.username} className="flex items-center gap-2">
+              <UserAvatar user={follower} /> {follower.username}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className="mt-4">
+        <h2 className="text-xl font-bold">Seguindo</h2>
+
+        {profile.following.length === 0 && <p>{profile.username} ainda não segue ninguém.</p>}
+
+        <ul className="mt-4 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
+          {profile.following.map(following => (
+            <li key={following.username} className="flex items-center gap-2">
+              <UserAvatar user={following} /> {following.username}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className="mt-4">
         <h2 className="text-xl font-bold">Trilhas inscritas</h2>
 
         {subscribedTrails.length === 0 && (
