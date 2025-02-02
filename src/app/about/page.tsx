3e9436d9930d05e68350f148f8145d96681de2e3
devlulcas <lucasalvesrego.contato@gmail.com>
2025-02-01 21:56:45 -0300

    feat: image ditter just for fun

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import coatiAvif from '@/shared/assets/images/coati.avif';
 import coatiSvg from '@/shared/assets/images/coati.svg';
+import { ditherImage } from '@/shared/lib/dittering';
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 export default function Page() {
   const links = {
@@ -8,37 +12,55 @@ export default function Page() {
     me: 'https://github.com/devlulcas/',
   };
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') ditherImage('coati')
+  }, [])
+
   return (
-    <main className="container flex min-h-[--view-height] flex-col justify-center gap-2 lg:flex-row lg:gap-8">
-      <div className="my-4 flex flex-col rounded-lg border bg-background/50 p-4 lg:w-1/2">
-        <h1 className="text-4xl font-bold">Sobre o projeto</h1>
+    <div className='bg-background/90'>
+      <main className="container relative h-[--view-height] grid" style={{ display: 'grid', gridTemplateAreas: "over" }}>
+        <div
+          style={{ gridArea: 'over' }}
+          className="h-[--view-height] w-full p-2"
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={coatiAvif.src}
+            width={coatiAvif.width}
+            height={coatiAvif.height}
+            alt="Coati"
+            id='coati'
+            className="rounded-lg object-cover h-full w-full"
+          />
+        </div>
 
-        <p className="prose my-8 text-justify text-lg text-white">
-          CoatiQuati é um projeto de{' '}
-          <a className="text-brand-400 hover:text-brand-300" href={links.github}>
-            código aberto
-          </a>
-          , desenvolvido por{' '}
-          <a className="text-brand-400 hover:text-brand-300" href={links.me}>
-            mim (Lucas Alves Rego)
-          </a>
-          , com o objetivo de ajudar pessoas interessadas em aprender algo novo e que não sabem por onde começar.
-        </p>
+        <div className="flex m-4 mt-auto h-fit items-center rounded-lg border bg-background/80 backdrop-blur-2xl p-4 gap-5" style={{ gridArea: 'over' }}>
+          <Image src={coatiSvg} alt="Coati" className="opacity-95" width={300} />
+          <div className='max-w-prose text-justify text-foreground'>
+            <h1 className="text-4xl font-bold">Sobre o projeto</h1>
 
-        <p>
-          <em className="font-semibold text-brand-400 hover:text-brand-300">Porque o nome CoatiQuati?</em>
-          <br />O quati é um animal que vive na América do Sul e Central, e é conhecido por ser um animal curioso e que
-          gosta de se aventurar. O nome CoatiQuati é uma brincadeira com o nome do animal em inglês e em português.
-        </p>
+            <p className="my-8">
+              CoatiQuati é um projeto de{' '}
+              <a className="text-brand-400 hover:text-brand-300" href={links.github}>
+                código aberto
+              </a>
+              , desenvolvido por{' '}
+              <a className="text-brand-400 hover:text-brand-300" href={links.me}>
+                mim (Lucas Alves Rego)
+              </a>
+              , com o objetivo de ajudar pessoas interessadas em aprender algo novo e que não sabem por onde começar.
+            </p>
 
-        <Image src={coatiSvg} alt="Coati" className="mx-auto mt-8 opacity-95" width={300} />
-      </div>
+            <p>
+              <em className="font-semibold text-brand-400 hover:text-brand-300">Porque o nome CoatiQuati?</em>
+              <br />O quati é um animal que vive na América do Sul e Central, e é conhecido por ser um animal curioso e que
+              gosta de se aventurar. O nome CoatiQuati é uma brincadeira com o nome do animal em inglês e em português.
+            </p>
 
-      <Image
-        src={coatiAvif}
-        alt="Coati"
-        className="my-4 rounded-lg object-cover grayscale filter transition-all duration-300 hover:grayscale-0 lg:w-1/2"
-      />
-    </main>
+          </div>
+        </div>
+      </main>
+    </div>
+
   );
 }
