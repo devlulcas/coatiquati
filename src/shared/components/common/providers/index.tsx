67d7af39a85c082434b1c3edbeacd4b7063db03e
devlulcas <lucasalvesrego.contato@gmail.com>
2025-02-01 21:57:35 -0300

    feat: use nuqs for search params handling

--- src/shared/components/common/providers/index.tsx
+++ src/shared/components/common/providers/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { useState } from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return <NuqsAdapter><QueryClientProvider client={queryClient}>{children}</QueryClientProvider></NuqsAdapter>;
 }
