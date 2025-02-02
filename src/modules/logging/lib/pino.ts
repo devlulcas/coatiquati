5906b8685dc9dfa07fa8684e5c7a641cb2ea9791
devlulcas <lucasalvesrego.contato@gmail.com>
2025-02-01 21:55:48 -0300

    feat: logging with pino-pretty in dev

--- src/modules/logging/lib/pino.ts
+++ src/modules/logging/lib/pino.ts
@@ -1,15 +1,3 @@
-import { PHASE_DEVELOPMENT_SERVER } from 'next/dist/shared/lib/constants';
 import { pino } from 'pino';
 
-const pretty = {
-  target: 'pino-pretty',
-  options: {
-    colorize: true,
-    ignore: 'pid,hostname',
-    levelFirst: true,
-  },
-};
-
-export const log = pino({
-  transport: PHASE_DEVELOPMENT_SERVER ? undefined : pretty,
-});
+export const log = pino();
